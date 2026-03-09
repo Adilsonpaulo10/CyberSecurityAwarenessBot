a35403037f03f395a4151b5a4da2ba82a1827761
Adilson Muzocola <ST10495060@rcconnect.edu.za>
2026-03-09 19:16:38 +0200

    Implemented voice greeting

---
+++ CyberSecurityAwarenessBot/VoiceGreeting.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Media;
+
+namespace CyberSecurityAwarenessBot
+{
+    class VoiceGreeting
+    {
+        public static void PlayGreeting()
+        {
+            try
+            {
+                SoundPlayer player = new SoundPlayer("greeting.wav");
+                player.PlaySync();
+            }
+            catch
+            {
+                Console.WriteLine("Voice greeting file not found.");
+            }
+        }
+    }
+}
